11c38ddfa407ac85707655fb43ceb8f9ed3015b7
StefanosTouf <stefetoufe@gmail.com>
2021-11-08 18:57:53 +0200

    Feature: session sql

--- db/create_entities.sql
+++ db/create_entities.sql
@@ -4,3 +4,11 @@ CREATE TABLE USER(
   password_hash VARCHAR(30) NOT NULL,
   access_token VARCHAR(30)
 )
+
+CREATE TABLE SESSION(
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  player1_id INT(6) UNSIGNED NOT NULL,
+  player2_id INT(6) UNSIGNED,
+  turn ENUM('p1', 'p2') NOT NULL,
+  winner ENUM('p1', 'p2', 'none') NOT NULL
+)
